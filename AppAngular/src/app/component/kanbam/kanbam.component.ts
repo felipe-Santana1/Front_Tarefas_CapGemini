412b8a71fd2ee0df43f2ba0f928e11d20a7877ce
Felipe Santana <felipe.santana2001@hotmail.com>
2024-10-28 19:14:58 -0300

    Table Tarefa dos Usuarios

--- AppAngular/src/app/component/kanbam/kanbam.component.ts
+++ AppAngular/src/app/component/kanbam/kanbam.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Tarefa } from '../tarefas/model/tarefa';
+import { TarefaService } from '../tarefas/tarefa.service';
 
 @Component({
   selector: 'app-kanbam',
   templateUrl: './kanbam.component.html',
   styleUrls: ['./kanbam.component.css']
 })
-export class KanbamComponent {
+export class KanbamComponent implements OnInit {
+
+
+  constructor(private service: TarefaService) {}
 
   tarefas: Tarefa[] = [];
+
+
+  ngOnInit(): void {
+    this.listarTarefas();
+  }
+  
+  listarTarefas(){
+    this.service.getAllTarefa().subscribe(
+      result => {
+        this.tarefas = result;
+        console.log(result)
+      }
+    )
+  }
 }
